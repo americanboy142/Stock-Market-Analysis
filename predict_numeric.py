3e549226ac4d6244879329c53a786f4a2bdaffa2
jack phreaner <jphreaner142@gmail.como>
2023-05-02 14:27:56 -0700

    no more plotting

--- predict_numeric.py
+++ predict_numeric.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def calculate_EMA_difference(short, long):
     """Calculates the difference between EMA short and long
